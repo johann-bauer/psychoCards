ac6fc429b17b4446b00b454184b34cc4a5f1f56a
Johann Bauer <johann.bauer1@gmail.com>
2010-01-03 20:58:40 +0100

    add GPL notes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+// This program is distributed under the terms of
+// the GNU General Public License version 3
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 #include <QtGui/QApplication>
 #include "widget.h"
 
